d13306c5603d089223e1929545bb96bc35f8625c
mohammeddawud <dawud.mohammed@turntabl.io>
2021-11-26 08:59:21 +0000

    adding id to get request

--- src/main/java/com/example/Test/TestController.java
+++ src/main/java/com/example/Test/TestController.java
@@ -1,12 +1,19 @@
 package com.example.Test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    public String index(){
-        return "My test is working on heroku";
+    @GetMapping("/test/{id}")
+    public String index(@PathVariable int id){
+         String status = null;
+        if(id==1){
+            status = "this is dawud";
+        }else if(id==2){
+            status = "this is edem";
+        }
+        return status;
     }
 }
