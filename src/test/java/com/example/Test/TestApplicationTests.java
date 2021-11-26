0d5e8675751fa3f4b895b877a080c73e4105c43e
eddykofi1500 <eddykofi1500@gmail.com>
2021-11-26 16:24:39 +0000

    testwork

--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.Test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,7 +16,7 @@ class TestApplicationTests {
 
 	@Test
 	public void shouldReturnMessage(){
-		
+		Assertions.assertEquals(1,1);
 	}
 
 }
