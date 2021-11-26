63cc40561714dea9f39c476c98a8efae5a0ddfd7
mohammeddawud <dawud.mohammed@turntabl.io>
2021-11-26 13:59:41 +0000

    check

--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.Test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void shouldReturnMessage(){
+		this.mockMvc.p
 	}
 
 }
